89805c654bd7faea756658a845a77a89eec757f8
Kalyan <kalyanprusty@gmail.com>
2022-07-22 13:06:44 +0000

    test1 uptodate

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -18,6 +18,7 @@ async def test_seq_bug1(dut):
     cocotb.start_soon(clock.start())        # Start the clock
 
     dut.inp_bit.value = 0 
+    clk_time = 10 #clock time period
 
     # reset
     dut.reset.value = 1
@@ -27,26 +28,104 @@ async def test_seq_bug1(dut):
     await FallingEdge(dut.clk)
 
     dut.inp_bit.value = 0   #0
+    time = 0
+    
     await FallingEdge(dut.clk) 
     dut.inp_bit.value = 0   #10
+    time = time + clk_time
+    
     await FallingEdge(dut.clk)
     dut.inp_bit.value = 0   #20
+    time = time + clk_time
+    
     await FallingEdge(dut.clk)
     dut.inp_bit.value = 1   #30
+    time = time + clk_time
+
     await FallingEdge(dut.clk)
     dut.inp_bit.value = 0   #40
+    time = time + clk_time
+
     await FallingEdge(dut.clk)
     dut.inp_bit.value = 1   #50
+    time = time + clk_time
+
     await FallingEdge(dut.clk)
     dut.inp_bit.value = 1   #60
+    time = time + clk_time
 
-    #actual output is in between 65 to 75
-    #expected output is between 75 to 85
-    await FallingEdge(dut.clk)
-    print(f"Now Time is 70 and output is {dut.seq_seen.value}")
-    await RisingEdge(dut.clk)
-    print(f"Now Time is 75 and output is {dut.seq_seen.value}")
-    await FallingEdge(dut.clk)
-    print(f"Now Time is 80 and output is {dut.seq_seen.value}")
+    #actual output is in between 65 to 75 : so check the value of state and output in between i.e. 70
+    #expected output is between 75 to 85 (as in the description it has to be delayed by one cycle) : so check the value of state and output in between i.e. 80
+    
+    await FallingEdge(dut.clk)  #70 
+    time = time + clk_time
+    print(f"Now Time is {time} and current_state is {dut.current_state.value}")
+    print(f"Now Time is {time} and output is {dut.seq_seen.value}")
 
+    await FallingEdge(dut.clk) #80
+    time = time + clk_time
+    print(f"Now Time is {time} and current_state is {dut.current_state.value}")
+    print(f"Now Time is {time} and output is {dut.seq_seen.value}")
+
+    assert (dut.seq_seen.value == 1) ,"output is {SEQ_SEEEN}, when current_state is {CUR_STATE}".format(SEQ_SEEEN = dut.seq_seen.value, CUR_STATE = dut.current_state.value)
+    cocotb.log.info('#### CTB: Develop your test here! ######')
+
+@cocotb.test()
+async def test_seq_bug2(dut):
+    """Test for seq detection """
+
+    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
+    cocotb.start_soon(clock.start())        # Start the clock
+
+    dut.inp_bit.value = 0 
+    clk_time = 10 #clock time period
+
+    # reset
+    dut.reset.value = 1
+    await FallingEdge(dut.clk)  
+    await FallingEdge(dut.clk)
+    dut.reset.value = 0
+    await FallingEdge(dut.clk)
+
+    dut.inp_bit.value = 0   #0
+    time = 0
+    
+    await FallingEdge(dut.clk) 
+    dut.inp_bit.value = 0   #10
+    time = time + clk_time
+    
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 0   #20
+    time = time + clk_time
+    
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 1   #30
+    time = time + clk_time
+
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 0   #40
+    time = time + clk_time
+
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 1   #50
+    time = time + clk_time
+
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 1   #60
+    time = time + clk_time
+
+    #actual output is in between 65 to 75 : so check the value of state and output in between i.e. 70
+    #expected output is between 75 to 85 (as in the description it has to be delayed by one cycle) : so check the value of state and output in between i.e. 80
+    
+    await FallingEdge(dut.clk)  #70 
+    time = time + clk_time
+    print(f"Now Time is {time} and current_state is {dut.current_state.value}")
+    print(f"Now Time is {time} and output is {dut.seq_seen.value}")
+
+    await FallingEdge(dut.clk) #80
+    time = time + clk_time
+    print(f"Now Time is {time} and current_state is {dut.current_state.value}")
+    print(f"Now Time is {time} and output is {dut.seq_seen.value}")
+
+    assert (dut.seq_seen.value == 1) ,"output is {SEQ_SEEEN}, when current_state is {CUR_STATE}".format(SEQ_SEEEN = dut.seq_seen.value, CUR_STATE = dut.current_state.value)
     cocotb.log.info('#### CTB: Develop your test here! ######')
