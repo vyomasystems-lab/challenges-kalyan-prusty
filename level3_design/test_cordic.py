c2de026d33753f00418c5069b0aa38da00d39605
Kalyan <kalyanprusty@gmail.com>
2022-07-27 18:35:00 +0000

    update make

--- level3_design/test_cordic.py
+++ level3_design/test_cordic.py
@@ -2,4 +2,23 @@ import os
 import random
 from pathlib import Path
 
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, FallingEdge
+
+@cocotb.test()
+async def test_cordic_bug1(dut):
+    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
+    cocotb.start_soon(clock.start())        # Start the clock
+
+    #defining all the inputs 
+    dut.Xin.value = 32000/1.647
+    dut.Yin.value = 0
+    dut.angle.value = 0x2eeeeeee # (2^32 * (66/360)) equivalent value for 66 degrees
+
+    #result will be available atlease after 16 clock cycles so we need to wait for it 
+    for i in range(18): # this number can be anything more than 16
+        await FallingEdge(dut.clk)
+    
+    print(f"SIN(66) = {dut.Xout.value} & COS(66) = {dut.Xout.value}")
 
