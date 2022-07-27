38fc57a2cc79b97f6cffd95ce86e3c47d9174035
Kalyan <kalyanprusty@gmail.com>
2022-07-27 18:53:15 +0000

    l3 correct design updated

--- level3_design/test_cordic.py
+++ level3_design/test_cordic.py
@@ -12,16 +12,21 @@ async def test_cordic_bug1(dut):
     cocotb.start_soon(clock.start())        # Start the clock
 
     #defining all the inputs 
-    dut.Xin.value = int(32000/1.647)
+    dut.Xin.value = int(32000/1.647) # 32000 is just a scaling factor and 1.647 is the gain of cordic algorithm
     dut.Yin.value = 0
     dut.angle.value = 0x2eeeeeee # (2^32 * (66/360)) equivalent value for 66 degrees
 
     #result will be available atlease after 16 clock cycles so we need to wait for it 
-    for i in range(18): # this number can be anything more than 16
+    for i in range(17): # this number can be anything more than 16
         await FallingEdge(dut.clock)
     
+    # getting the exact value of output 
     cos_val = int(str(dut.Xout.value),2)
     sin_val = int(str(dut.Yout.value),2)
-
     print(f"COS(66) = {cos_val} & SIN(66) = {sin_val}")
+
+    #converting the output into [-1,1] range
+    cos_exact = cos_val/32000
+    sin_exact = sin_val/32000
+    print(f"COS(66) = {cos_exact} & SIN(66) = {sin_exact}")
 
